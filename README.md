bfeb1713c00743127730aedce6c990b58a375025
Sadam·Sadik <1903249375@qq.com>
2024-04-25 13:09:38 +0800

    Add some TODO , and make some base configuration.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # FlowPilot
+
 A net flow pilot in order to handle some proxy configuration automatically.
+
 ### TODO
+
 * [ ] Make this way automatic by using the applescript.
+* [ ] 根据地理位置确定忽略哪些地址走代理.
--- main.py
+++ main.py
@@ -20,4 +20,7 @@ if __name__ == '__main__':
             ignores_list.extend(_list)
     final = list(set(ignores_list))
     print("final:", final)
-    print("final_str:", ",".join(final))
+    final_str = ",".join(final)
+    print("final_str:", final_str)
+    # 控制台不支持范型和通配符, 必须是确切的域名
+    print(f'export no_proxy="{final_str}"')
