cf2dcf8774826d8a615c04d39cd42efa9480cde1
Sadam·Sadik <1903249375@qq.com>
2024-04-28 14:01:38 +0800

    实现了通过配置文件来对upstream配置进行持久化.实现了网络服务名单的自动获取.
    Signed-off-by: Sadam·Sadik <1903249375@qq.com>

--- FlowPilot-cli.py
+++ FlowPilot-cli.py
@@ -10,13 +10,27 @@ import json
 import logging
 import os
 
+import click
+
 from utils import logger
 from utils.proxy_helper.macosx import set_web_proxy, set_cmd_proxy
 
-if __name__ == '__main__':
-    logger.init("FlowPilot", console_level=logging.INFO)
-    host = "10.2.1.0"
-    port = 7890
+
+@click.command(help="Command line interface for FlowPilot")
+def main():
+    config_fp = os.path.expanduser("~/.flowPilot/config.json")
+    if not os.path.isfile(config_fp):
+        click.echo("upstream config file: {} does not exist.".format(config_fp))
+        host = click.prompt("Input the proxy upstream host")
+        port = click.prompt("Input the proxy upstream port")
+        config = {"host": host, "port": port}
+        os.makedirs(os.path.dirname(config_fp), exist_ok=True)
+        with open(config_fp, "w", encoding='utf-8') as f:
+            json.dump(config, f, ensure_ascii=False)
+    else:
+        config = json.load(open(config_fp))
+    host = config["host"]
+    port = config["port"]
     ignores_list = []
     ignores_fps = os.listdir('ignores')
     for fn in ignores_fps:
@@ -28,7 +42,8 @@ if __name__ == '__main__':
     bypass_domains = list(set(ignores_list))
     set_web_proxy(host, port, bypass_domains)
     set_cmd_proxy(host, port, bypass_domains)
-    # print("export http_proxy={}".format(host))
-    # print("export https_proxy={}".format(port))
-    # # 控制台不支持范型和通配符, 必须是确切的域名
-    # print(f'export no_proxy="%s"' % (",".join(bypass_domains)))
+
+
+if __name__ == '__main__':
+    logger.init("FlowPilot", console_level=logging.INFO)
+    main()
--- utils/proxy_helper/macosx.py
+++ utils/proxy_helper/macosx.py
@@ -11,45 +11,65 @@ import subprocess
 import os
 
 
-def set_cmd_proxy(host, port, bypass_domains: list[str], env_rc_file: str = ".zshrc"):
+def set_cmd_proxy(host, port, bypass_domains: list[str], env_rc_files: list[str] = [".zshrc", ".bashrc"]):
     # 新的代理服务器设置
     http_proxy_url = "http://{}:{}".format(host, port)
     https_proxy_url = "http://{}:{}".format(host, port)
     no_proxy_domains = ",".join(bypass_domains)
+    for env_rc_file in env_rc_files:
+        # 要编辑的文件路径
+        env_rc_fp = os.path.expanduser(f"~/{env_rc_file}")
 
-    # 要编辑的文件路径
-    zshrc_path = os.path.expanduser(f"~/{env_rc_file}")
-
-    # 检查并读取文件内容
-    if not os.path.isfile(zshrc_path):
-        with open(zshrc_path, "w") as f:
+        # 检查并读取文件内容
+        if not os.path.isfile(env_rc_fp):
             pass
-    else:
-        with open(zshrc_path, "r+") as zshrc_file:
-            lines = zshrc_file.readlines()
-            updated_lines = []
+        else:
+            with open(env_rc_fp, "r+") as zshrc_file:
+                lines = zshrc_file.readlines()
+                updated_lines = []
 
-            # 查找并移除旧的代理设置
-            for line in lines:
-                if not (
-                        ("http_proxy=" in line)
-                        or ("https_proxy=" in line)
-                        or ("no_proxy=" in line)
-                ):
-                    updated_lines.append(line)
+                # 查找并移除旧的代理设置
+                for line in lines:
+                    if not (
+                            ("http_proxy=" in line)
+                            or ("https_proxy=" in line)
+                            or ("no_proxy=" in line)
+                    ):
+                        updated_lines.append(line)
 
-            # 添加新的代理设置
-            updated_lines.append("\n# 更新代理设置\n")
-            updated_lines.append(f"export http_proxy=\"{http_proxy_url}\"\n")
-            updated_lines.append(f"export https_proxy=\"{https_proxy_url}\"\n")
-            updated_lines.append(f"export no_proxy=\"{no_proxy_domains}\"\n")
+                # 添加新的代理设置
+                updated_lines.append("\n# 更新代理设置\n")
+                updated_lines.append(f"export http_proxy=\"{http_proxy_url}\"\n")
+                updated_lines.append(f"export https_proxy=\"{https_proxy_url}\"\n")
+                updated_lines.append(f"export no_proxy=\"{no_proxy_domains}\"\n")
 
-            # 将更新后的行重写回文件
-            zshrc_file.seek(0)
-            zshrc_file.truncate()
-            zshrc_file.writelines(updated_lines)
+                # 将更新后的行重写回文件
+                zshrc_file.seek(0)
+                zshrc_file.truncate()
+                zshrc_file.writelines(updated_lines)
 
-        logging.info("代理设置已在.zshrc文件中更新，请运行 'source ~/.zshrc' 以应用新的环境变量设置。")
+            logging.info(f"代理设置已在{env_rc_file}文件中更新，请运行 'source ~/{env_rc_file}' 以应用新的环境变量设置。")
+
+
+def get_network_services():
+    """
+    利用 networksetup的 -listallnetworkservices 参数来获取并自动遍历出 networservices.
+    """
+
+    try:
+        # 调用networksetup命令并使用-listallnetworkservices选项来列出所有网络服务
+        output = subprocess.check_output(["networksetup", "-listallnetworkservices"])
+        # 将输出从字节串转换为字符串，并按行分割
+        network_services = output.decode("utf-8").strip().split("\n")
+        # 过滤掉可能的空行
+        res = []
+        for service in network_services:
+            if service and "is disabled" not in service:
+                res.append(service)
+        return res
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return []
 
 
 def set_web_proxy(host, port: int, bypass_domains: list[str]):
@@ -60,8 +80,10 @@ def set_web_proxy(host, port: int, bypass_domains: list[str]):
     :param bypass_domains:
     :return:
     """
-    # TODO: 实现 利用 networksetup的 -listallnetworkservices 参数来获取并自动遍历出 networservices.
-    for service in ["Wi-Fi", "USB 10/100 LAN", "USB 10/100 LAN 2", "USB 10/100 LAN 3"]:
+    # 利用 networksetup的 -listallnetworkservices 参数来获取并自动遍历出 networservices.
+    network_services = get_network_services()
+    logging.info(f"network services:{network_services}")
+    for service in network_services:
         for proxy_type in ["webproxy", "securewebproxy", "socksfirewallproxy"]:
             cmd = ["/usr/sbin/networksetup", "-set" + proxy_type, service, host, str(port)]
             resp = subprocess.run(cmd, check=True)
