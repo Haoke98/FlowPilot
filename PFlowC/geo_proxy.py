bffd6a609e5438811f687c8cbfe9a6eadead0db1
Sadam·Sadik <1903249375@qq.com>
2024-05-09 19:26:30 +0800

    增加了针对一部分域名固定引流到上游流量节点
    Signed-off-by: Sadam·Sadik <1903249375@qq.com>

--- PFlowC/geo_proxy.py
+++ PFlowC/geo_proxy.py
@@ -30,8 +30,14 @@ DEFAULT_BYPASS_DOMAINS = [
     '223.5.5.5',
     '119.29.29.29',
     'doh.pub',
-    'dns.alidns.com'
+    'dns.alidns.com',
+    "218.31.113.0/24"
 ]
+AGENT_DOMAINS = {
+    "CN": [
+        "github.com"
+        "api.github.com"]
+}
 # 初始化GeoIP数据库
 geoip_db = geoip2.database.Reader(GEOIP_DB_PATH)
 home_dir = os.path.expanduser("~/.PFlowC")
@@ -71,14 +77,17 @@ def request(flow: http.HTTPFlow) -> None:
     # 这里通过域名服务获取IP地址的方法要改成从特定的DNS获取, 直接从socket获取可能会因为不同的本地环境而导致出现异常.
     # 判断是否属于本地区域
     ip = socket.gethostbyname(flow.request.pretty_host)
-    if is_domestic2(flow.request.pretty_host, target_country_code="CN"):
-        # 直接访问，不走上游代理
-        print(f"[{flow.timestamp_start}][{flow.type}][mode:{flow.mode}][DIRECT][{ip} - {flow.request.pretty_host}]")
-        # 更新一下bypass_domains列表
-        # TODO: 部分特殊域名要存在于官网规则中, 对他们进行过滤单独分离出来
-        bypass_domains.add(flow.request.pretty_host)
-        set_bypass_domains(bypass_domains)
-        save_bypass_domains()
-        # TODO: 收集到中央服务方便以后成为按地区分布代理流量控制缓存.
+    _type = "PROXY"
+    if flow.request.pretty_host in AGENT_DOMAINS[LOCAL_REGION_CODE]:
+        _type = "PROXY"
     else:
-        print(f"[{flow.timestamp_start}][{flow.type}][mode:{flow.mode}][PROXY][{ip} - {flow.request.pretty_host}]")
+        # 直接访问，不走上游代理
+        _type = "DIRECT"
+        if is_domestic2(flow.request.pretty_host, target_country_code=LOCAL_REGION_CODE):
+            # 需要更新一下bypass_domains列表
+            # TODO: 部分特殊域名要存在于官网规则中, 对他们进行过滤单独分离出来
+            bypass_domains.add(flow.request.pretty_host)
+            set_bypass_domains(bypass_domains)
+            save_bypass_domains()
+            # TODO: 收集到中央服务方便以后成为按地区分布代理流量控制缓存.
+    print(f"[{flow.timestamp_start}][{flow.type}][mode:{flow.mode}][{_type}][{ip} - {flow.request.pretty_host}]")
