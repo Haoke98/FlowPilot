402761d983564b87824a89327553d0e8ddc9e7ed
Sadam·Sadik <haoke98@outlook.com>
2026-07-30 22:44:33 +0800

    fix: -d 模式下将 logging 级别设为 DEBUG，使鉴权日志可见
    v4.0.1 中 debug 日志用 logging.debug() 但 logger 级别是 INFO，被过滤

--- PFlowC/router.py
+++ PFlowC/router.py
@@ -454,6 +454,8 @@ def start(listen_port=None, mode=None, upstreams=None,
 
     # Debug 模式
     DEBUG = debug
+    if DEBUG:
+        logging.getLogger().setLevel(logging.DEBUG)
 
     async def _serve():
         server = await asyncio.start_server(_handle, LISTEN_HOST, LISTEN_PORT)
